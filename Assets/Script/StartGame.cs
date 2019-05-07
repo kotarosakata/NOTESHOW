8439609d91449daf621234bde95f2e8e756f2edf
kotarousakata <50367628+kotarousakata@users.noreply.github.com>
2019-05-07 22:52:48 +0900

    最初です

---
+++ Assets/Script/StartGame.cs
@@ -0,0 +1,35 @@
+﻿using  System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public AudioSource _audioSource;
+    public AudioSource _AudioSource2;
+
+
+    private void Start()
+    {
+                Time.timeScale = 1.0f;
+    }
+
+    void Update()
+    {
+        if (Input.touchCount>0)
+        {
+            StartCoroutine(_coroutine());
+        }
+    }
+
+    IEnumerator _coroutine()
+    {
+
+        _AudioSource2.Stop();
+        _audioSource.Play();
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("GameScene");
+    }
+
+
+}
---
+++ Assets/Script/move.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class move : MonoBehaviour
+{
+    private int movespeed;
+    private GameObject _gameObject;
+    private bool enter=true;
+
+    void Start()
+    {
+        _gameObject = GameObject.Find("GameMusic");
+        
+    }
+    void Update()
+    {
+        if (Time.timeScale==1.0f&&enter)
+        {
+            movespeed = _gameObject.GetComponent<GameController>().notesspeed;
+            enter = false;
+        }
+        transform.Translate(-transform.forward*Time.deltaTime*movespeed);
+    }
+    
+
+    public void startpoint(int a)
+    {
+        if (a ==0)
+        {
+            transform.position =new Vector3(-7.5f,0,224.5f);
+        }else if(a ==1)
+        {
+            transform.position =new Vector3(-2.5f,0,224.5f);
+
+        }else if (a == 2)
+        {
+            transform.position =new Vector3(2.5f,0,224.5f);
+        }
+        else if(a==3)
+        {
+            transform.position =new Vector3(7.5f,0,224.5f);
+
+        }
+        else 
+        {
+            transform.position = new Vector3(0,0,207.0f);
+        }
+
+        
+    }
+
+    public void Onclick()
+    {
+        Debug.Log("onclick");
+        gameObject.SetActive(false);
+    }
+
+
+
+
+}
